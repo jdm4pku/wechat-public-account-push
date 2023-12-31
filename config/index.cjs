8b898cb096b47d98898fbe62da780ac10c767801
Dongming <148830652+jdm4pku@users.noreply.github.com>
2023-12-31 11:09:06 +0800

    Update index.cjs
    add newyear

--- config/index.cjs
+++ config/index.cjs
@@ -21,20 +21,13 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '宝宝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'olX526m81jo7LMgMWLohh1SE-LOY',
+      // id: 'olX526m81jo7LMgMWLohh1SE-LOY',
+      id: 'olX526s6iPRuaaPMBFQeun5K63So',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'Epny9BUMNY1oW-cNk9jjlNNIbyB0aKOEpGWOW6JljmY',
+      useTemplateId: 'kZWCbohKX4zuQSNJwWaBmh4Gqooc6tnwwZ5Hv2neJXg',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '11-14',
       festivals: [
-        // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
-        {
-          type: '*生日', name: '宝贝', year: '1996', date: '09-09',
-        },
-        // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
-        {
-          type: '生日', name: '李四', year: '1996', date: '09-31',
-        },
         {
           type: '节日', name: '相识纪念日', year: '2023', date: '10-04',
         },
@@ -43,8 +36,6 @@ const USER_CONFIG = {
       customizedDateList: [
         // 在一起的日子
         { keyword: 'love_day', date: '2023-12-02' },
-        // // 结婚纪念日
-        // { keyword: 'marry_day', date: '2022-09-09' },
       ],
     },
   ],
@@ -57,7 +48,8 @@ const USER_CONFIG = {
     {
       name: '宝宝',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'olX526m81jo7LMgMWLohh1SE-LOY',
+      // id: 'olX526m81jo7LMgMWLohh1SE-LOY',
+      id: 'olX526s6iPRuaaPMBFQeun5K63So',
     }
   ],
 
