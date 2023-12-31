7037eb73ccf0c31ec817ac2d894f9ec986ab42b3
Dongming <148830652+jdm4pku@users.noreply.github.com>
2023-12-31 11:23:47 +0800

    Update index.cjs
    test push success

--- config/index.cjs
+++ config/index.cjs
@@ -21,8 +21,8 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '宝宝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'olX526m81jo7LMgMWLohh1SE-LOY',
-      // id: 'olX526s6iPRuaaPMBFQeun5K63So',
+      // id: 'olX526m81jo7LMgMWLohh1SE-LOY',
+      id: 'olX526s6iPRuaaPMBFQeun5K63So',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'kZWCbohKX4zuQSNJwWaBmh4Gqooc6tnwwZ5Hv2neJXg',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
@@ -42,7 +42,7 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '',
+  CALLBACK_TEMPLATE_ID: 'tgFCxZkGoSmKUpw6eguSm6k8q3GIRtm6ZL2vOVmhxl4',
 
   CALLBACK_USERS: [
     {
